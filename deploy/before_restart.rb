065eb663e4f81ebf9425f8aac812abc8201ec295
William Zhang <william_zhang@schumachergroup.com>
2014-08-07 11:12:02 -0500

    Removed Chef Recipe

--- deploy/before_restart.rb
+++
@@ -1,9 +0,0 @@
-Chef::Log.info("Running deploy/before_restart.rb")
-
-# map the environment_variables node to ENV
-node[:deploy].each do |application, deploy|
-  deploy[:environment_variables].each do |key, value|
-    Chef::Log.info("Setting ENV[#{key}] to #{value}")
-    ENV[key] = value
-  end
-end	
